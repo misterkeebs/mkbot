180ed7789055a4e95cdb529fda2a2a612f013d61
Felipe Coury <felipe.coury@gmail.com>
2019-11-14 14:03:19 -0300

    feat: redesigned Hero

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 import logo from "../assets/logo.png";
+import demo from "../assets/demo.gif";
 
 const Hero = () => (
-  <div className="text-center hero my-5">
+  <div className="text-center hero my-3">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">MrKeebs MKBot Artisan Discord Bot</h1>
+    <h1 className="mb-4">MKBot Artisan Discord Bot</h1>
+
+    <p>
+      <img src={demo} width="400"/>
+    </p>
 
     <p className="lead">
       Allows Discord users to research makers, sculpts, colorways and
