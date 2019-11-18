fbd2f445fa6548f56a7db5193943993be0bad600
Felipe Coury <felipe.coury@gmail.com>
2019-11-18 09:26:52 -0300

    fix: lists uses user_id instead of discord_user_id

--- server/list.js
+++ server/list.js
@@ -9,14 +9,14 @@ class ListRoutes extends RouterConfig {
 
   async removeArtisan(req, res, next) {
     const { type, artisan_id } = req.params;
-    const list = await List.findByUser(this.client, type, this.user.discord_user_id);
+    const list = await List.findByUser(this.client, type, this.user.user_id);
     const response = await list.remove(artisan_id);
     res.json({ deleted: response && response.rowCount > 0 });
   }
 
   async addArtisan(req, res, next) {
     const { type, artisan_id } = req.params;
-    const list = await List.findOrCreate(this.client, type, this.user.discord_user_id);
+    const list = await List.findOrCreate(this.client, type, this.user.user_id);
     const response = await list.add({ artisan_id });
     console.log(' *** response', response);
     res.json({ added: response && response.rowCount > 0 });
--- server/user.js
+++ server/user.js
@@ -20,7 +20,7 @@ class UserRoutes extends RouterConfig {
   async getUserList(type, req, res, next) {
     const { userProfile } = req;
     const user = await User.findOrCreate(this.client, { email: userProfile.email });
-    const list = await List.findByUser(this.client, type, user.discord_user_id);
+    const list = await List.findByUser(this.client, type, user.user_id);
     console.log(' *** list', list);
     if (!list) {
       return res.json(null);
