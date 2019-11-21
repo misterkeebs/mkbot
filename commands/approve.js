49a2a1735e409dddc380a8e74947f63111fd9420
Felipe Coury <felipe.coury@gmail.com>
2019-11-20 23:13:29 -0300

    chore: formatting

--- commands/approve.js
+++ commands/approve.js
@@ -17,17 +17,17 @@ module.exports = async function(client, msg, content) {
   }
 
   const maker = await findMaker(client, data.maker);
-      if (!maker) {
-        return await msg.reply(dedent`
-        ERROR: Maker **${data.maker}** was not found.
+  if (!maker) {
+    return await msg.reply(dedent`
+    ERROR: Maker **${data.maker}** was not found.
 
-        - to add it, use \`!addm ${data.maker}\`
-        - to fix the maker name, use \`!fix maker ${data.submission_id} [maker name]\`
-        - to reject this submission, use \`!dec ${data.submission_id}\`
+    - to add it, use \`!addm ${data.maker}\`
+    - to fix the maker name, use \`!fix maker ${data.submission_id} [maker name]\`
+    - to reject this submission, use \`!dec ${data.submission_id}\`
 
-        After taking action, you can approve this entry with \`!app ${data.submission_id}\`
-        `);
-      }
+    After taking action, you can approve this entry with \`!app ${data.submission_id}\`
+    `);
+  }
 
   data.maker_id = maker.maker_id;
 
