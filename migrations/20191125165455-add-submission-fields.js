9546d7798b35c04eaa016c848e6679c5c5f2452f
Felipe Coury <felipe.coury@gmail.com>
2019-11-25 13:59:43 -0300

    feat: allow submitter to decide how he wants to be credited, if at all (wip)

---
+++ migrations/20191125165455-add-submission-fields.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return Promise.all([
+    db.addColumn('submissions', 'anonymous', 'boolean'),
+    db.addColumn('submissions', 'author', 'string'),
+  ]);
+};
+
+exports.down = function(db) {
+  return Promise.all([
+    db.dropColumn('submissions', 'anonymous'),
+    db.dropColumn('submissions', 'author'),
+  ]);
+};
+
+exports._meta = {
+  "version": 1
+};
