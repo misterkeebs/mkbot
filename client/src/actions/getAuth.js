2b7143991601c6004c7f7843540d744a8da8d6db
Felipe Coury <felipe.coury@gmail.com>
2019-11-27 14:20:15 -0300

    fix: wishlist and list URLs

--- client/src/actions/getAuth.js
+++ client/src/actions/getAuth.js
@@ -1,6 +1,6 @@
 export default (getTokenSilently, path) => async _ => {
   const token = await getTokenSilently();
-  const response = await fetch(`/api/${path}`, {
+  const response = await fetch(`/api${path}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     }
