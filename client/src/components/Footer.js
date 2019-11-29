253162972426ae6d6db13a9c6fed3c0d81f948fe
Felipe Coury <felipe.coury@gmail.com>
2019-11-28 22:32:48 -0300

    chore: uses http for mrkeebs.com

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <div className="logo" />
     <p>
-      Brought to you by <a href="https://mrkeebs.com">MrKeebs</a>.
+      Brought to you by <a href="http://mrkeebs.com">MrKeebs</a>.
       Fork me on <a href="https://github.com/misterkeebs/mkbot">GitHub</a>.
     </p>
   </footer>
